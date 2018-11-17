665b9dc2a252b178f2304cf3eef95f4c6025f948
crazylong <316677841@qq.com>
2018-11-17 08:34:43 +0800

    for

--- scala/src/main/scala/impatient/chapter2.scala
+++ scala/src/main/scala/impatient/chapter2.scala
@@ -1,10 +1,17 @@
 package impatient
 
+import org.junit.Test
+
 /**
   *
   * @author Long Qiong 
   * @create 2018/11/15
   */
 class chapter2 {
-
+  @Test
+  def testFor1(): Unit ={
+    for(i < -1 to 10){
+      println(i+)
+    }
+  }
 }
