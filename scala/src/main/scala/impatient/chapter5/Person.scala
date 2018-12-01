f429055b9a6989d13eb8552578a4d9dc5dac2a5a
crazylong <316677841@qq.com>
2018-12-01 22:09:21 +0800

    chapter4

--- scala/src/main/scala/impatient/chapter5/Person.scala
+++ scala/src/main/scala/impatient/chapter5/Person.scala
@@ -1,6 +1,16 @@
 package impatient.chapter5
 
+import scala.beans.BeanProperty
+
 /**
+  *在事项属性时你有如下四个选择：
+  * 1，var foo: Scala自动合成一个getter方法和一个setter方法
+  * 2, val foo:Scala自动合成一个getter方法
+  * 3，由你来定义foo 和 foo_= 方法
+  * 4，由你来定义foo 方法
+  *
+  * 在Scala中，你不能实现只写属性
+  * （即带有setter但不带getter的属性）
   *
   * @author Long Qiong 
   * @create 2018/12/1
@@ -21,18 +31,47 @@ class Person {
 
   //重新定义getter和setter方法
   private var privateSex = 0
-  def sex = privateSex   //getter
 
-  def sex_(newValue : Int)={//setter
-    if(newValue > 3) privateSex = 1
+  def sex = privateSex //getter
+
+  def sex_(newValue: Int) = {
+    //setter
+    if (newValue > 3) privateSex = 1
   }
 
   //如果字段是私有的，则getter和setter方法也是私有的
   //如果字段是val， 则只有getter方法被生成
   //如果你不需要任何getter或setter，可以将字段声明为private[this]
 
-
+  //会生成一个私有的final字段和一个getter方法
   val school = ""
 
-  private[this] val score = 0
+  private var score1 = 0
+
+  //对象私有
+  private[this] val score2 = 0
+
+  //方法可以访问该类的所有对象的私有字段
+  def isLess(other: Person) = score1 < other.score1 //访问另一个对象的私有字段
+
+  //private[this] 只能访问当前对象的私有字段
+  //def isLess2(other: Person) = score2 < other.score2
+
+  //Scala允许你将访问权限赋予指定的类
+  //private[类名]修饰符可以定义仅有指定类的方法可以访问给定的字段
+  //这里的类名必须时当前定义的类，或者是包含该类的外部类
+
+
+  //Bean 属性
+  @BeanProperty var last_name: String = _
+  //将会生成四个方法：
+  //1. name: String
+  //2. name_ =(newValue:String):Unit
+  //3. getName():String
+  //4. setName(newValue:String):Unit
+
+  //如果你以主构造器参数的方式定义了某字段，并且你需要JavaBeans版的getter和setter方法，
+  //像如下这样给构造器参数加上注解即可：
+  //class Person(@BeanProperty var name: String)
+
 }
