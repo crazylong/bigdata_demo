e6248ec91e37fb27e16f543aa11c1bcdecc6b116
crazylong <316677841@qq.com>
2018-12-06 23:30:51 +0800

    chapter6

--- scala/src/main/scala/impatient/chapter6/TestChapter6.scala
+++ scala/src/main/scala/impatient/chapter6/TestChapter6.scala
@@ -12,4 +12,25 @@ class TestChapter6 {
   def testApply(): Unit ={
     val acct = Account(1000.0)
   }
+
+  @Test
+  def testEnum(): Unit ={
+    println(doWhat(TrafficLightColor.Red))
+
+    for(c <- TrafficLightColor.values)
+      println(s"${c.id}: $c")
+
+    //通过枚举的ID或名称来进行查找定位
+    println(TrafficLightColor(0)) //将调用Enumeration.apply
+    //println(TrafficLightColor.withName("Red"))
+    println(TrafficLightColor.withName("Stop"))
+  }
+  import TrafficLightColor._
+  //如果没有使用类型别名(type TrafficLightColor = Value)下面方法定义只能使用：
+  //def doWhat(color: TrafficLightColor.Value)={ }
+  def doWhat(color: TrafficLightColor)={
+    if(color == Red) "Stop"
+    else if(color == Yellow) "hurry up"
+    else "go"
+  }
 }
