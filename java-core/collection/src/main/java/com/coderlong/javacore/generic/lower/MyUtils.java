c3947fa04a5cba95974919b15fcb02787eb935f6
coderlong <316677841@qq.com>
2019-04-04 18:02:53 +0800

    泛型方法

--- java-core/collection/src/main/java/com/coderlong/javacore/generic/lower/MyUtils.java
+++ java-core/collection/src/main/java/com/coderlong/javacore/generic/lower/MyUtils.java
@@ -8,7 +8,18 @@ import java.util.List;
  * @create 2019/3/28
  */
 public class MyUtils {
+    //通配符下限
     <T> T copy(List<? super T> dest, List<T> src){
+        T last = null;
+        for (T t : src) {
+            last = t;
+            dest.add(t);
+        }
+        return last;
+    }
+
+    //通配符上限
+    <T> T copy2(List<T> dest, List<? extends T> src){
         T last = null;
         for (T t : src) {
             last = t;
