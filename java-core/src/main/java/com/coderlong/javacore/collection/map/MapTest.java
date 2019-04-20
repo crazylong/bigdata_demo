2a54ee207a4fca0abca1680e8fcbd47afd026506
coderlong <316677841@qq.com>
2019-04-21 07:03:42 +0800

    MapTest

---
+++ java-core/src/main/java/com/coderlong/javacore/collection/map/MapTest.java
@@ -0,0 +1,16 @@
+package com.coderlong.javacore.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Long Qiong
+ * @create 2019/4/18
+ */
+public class MapTest {
+    public void testMap(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("java", "100");
+
+    }
+}
