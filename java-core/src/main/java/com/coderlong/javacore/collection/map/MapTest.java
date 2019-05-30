bb171fb1bdaa8d776baa61838d3fec9c1c14ba28
coderlong <316677841@qq.com>
2019-05-30 17:52:53 +0800

    java_core

--- java-core/src/main/java/com/coderlong/javacore/collection/map/MapTest.java
+++ java-core/src/main/java/com/coderlong/javacore/collection/map/MapTest.java
@@ -1,6 +1,9 @@
 package com.coderlong.javacore.collection.map;
 
+import org.junit.Test;
+
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 /**
@@ -12,5 +15,14 @@ public class MapTest {
         HashMap<String, String> map = new HashMap<>();
         map.put("java", "100");
 
+        Hashtable ht = new Hashtable();
+
+    }
+
+    @Test
+    public void test01(){
+
+        System.out.println(1 << 4);
+        System.out.println(1 << 30);
     }
 }
