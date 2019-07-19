be0182ced9b53be0da400c624c7484d99a3f6021
crazylong <316677841@qq.com>
2019-07-19 14:24:58 +0800

    mongodb

---
+++ mongodb/src/main/java/com/coderlong/mongodb/MockData.java
@@ -0,0 +1,14 @@
+package com.coderlong.mongodb;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MockData {
+    public static void main(String[] args) {
+        ExecutorService poolExecutor = Executors.newFixedThreadPool(3);
+
+
+    }
+
+
+}
---
+++ mongodb/src/main/java/com/coderlong/mongodb/NBGTRUCKGPS.java
@@ -0,0 +1,28 @@
+//package com.coderlong.mongodb;
+//
+//public class NBGTRUCKGPS {
+//    private String CAR_NO;
+//    private String CAR_AREA1;
+//    private String CAR_AREA2;
+//    private String CAR_CALL_ID;
+//    private String CAR_COLOR;
+//    private String GPS_DATA_TYPE;
+//    private String GPS_FROM;
+//    private String GNSSCENTERID;
+//    private String GPS_ALARM;
+//    private String GPS_ALTITUDE;
+//    private String GPS_CMB_DATE;
+//    private String GPS_DIRECTIO;
+//    private String GPS_EXCRYPT;
+//    private String GPS_LAT;
+//    private String GPS_LON;
+//    private String ;
+//    private String ;
+//    private String ;
+//    private String ;
+//    private String ;
+//    private String ;
+//    private String ;
+//    private String ;
+//    private String ;
+//}
