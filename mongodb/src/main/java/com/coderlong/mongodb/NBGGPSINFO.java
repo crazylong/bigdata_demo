06f4e35358dfd30dab7f5e159865dc76848fc328
crazylong <316677841@qq.com>
2019-07-25 10:57:58 +0800

    mongodb

---
+++ mongodb/src/main/java/com/coderlong/mongodb/NBGGPSINFO.java
@@ -0,0 +1,175 @@
+package com.coderlong.mongodb;
+
+public class NBGGPSINFO {
+    private String no;
+    private String area1;
+    private String area2;
+    private String color;
+    private String data_type;
+    private String gnsscenterid;
+    private String alarm;
+    private String altitude;
+    private String cmb_date;
+    private String direction;
+    private String excrypt;
+    private String lat;
+    private String lon;
+    private String state;
+    private String time;
+    private String vec1;
+    private String vec2;
+    private String vec3;
+    private String type	;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getArea1() {
+        return area1;
+    }
+
+    public void setArea1(String area1) {
+        this.area1 = area1;
+    }
+
+    public String getArea2() {
+        return area2;
+    }
+
+    public void setArea2(String area2) {
+        this.area2 = area2;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getData_type() {
+        return data_type;
+    }
+
+    public void setData_type(String data_type) {
+        this.data_type = data_type;
+    }
+
+    public String getGnsscenterid() {
+        return gnsscenterid;
+    }
+
+    public void setGnsscenterid(String gnsscenterid) {
+        this.gnsscenterid = gnsscenterid;
+    }
+
+    public String getAlarm() {
+        return alarm;
+    }
+
+    public void setAlarm(String alarm) {
+        this.alarm = alarm;
+    }
+
+    public String getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(String altitude) {
+        this.altitude = altitude;
+    }
+
+    public String getCmb_date() {
+        return cmb_date;
+    }
+
+    public void setCmb_date(String cmb_date) {
+        this.cmb_date = cmb_date;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getExcrypt() {
+        return excrypt;
+    }
+
+    public void setExcrypt(String excrypt) {
+        this.excrypt = excrypt;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getVec1() {
+        return vec1;
+    }
+
+    public void setVec1(String vec1) {
+        this.vec1 = vec1;
+    }
+
+    public String getVec2() {
+        return vec2;
+    }
+
+    public void setVec2(String vec2) {
+        this.vec2 = vec2;
+    }
+
+    public String getVec3() {
+        return vec3;
+    }
+
+    public void setVec3(String vec3) {
+        this.vec3 = vec3;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
