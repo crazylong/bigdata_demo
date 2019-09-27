e9902c3da47d426d2c6bccdf644f3bbbfae2fd6f
crazylong <316677841@qq.com>
2019-09-27 10:54:42 +0800

    dateutil

--- util/src/main/java/com/coderlong/util/DateUtil.java
+++ util/src/main/java/com/coderlong/util/DateUtil.java
@@ -2,7 +2,9 @@ package com.coderlong.util;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
@@ -14,6 +16,23 @@ public class DateUtil {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
 
         System.out.println(sdf.format(new Date(dateTime)));
+    }
+
+    @Test
+    public void test02(){
+        Calendar calendar = Calendar.getInstance(); // java.util包
+        calendar.add(Calendar.DATE, -1); // 如果不够减会将月变动
+
+        Date date = calendar.getTime();
+        SimpleDateFormat sdfTime = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
+        String dateStr = sdfTime.format(date);
+
+        try {
+            calendar.setTime(sdfTime.parse(dateStr));
+            System.out.println(calendar.getTimeInMillis());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 }
