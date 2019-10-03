1621c3fb1d8b56bc0d6b4c5e0a235ee7c6c8fd04
crazylong <316677841@qq.com>
2019-10-03 21:22:05 +0800

    字节码

---
+++ java-core/src/main/java/com/coderlong/bytecode/MyConstantTest.java
@@ -0,0 +1,9 @@
+package com.coderlong.bytecode;
+
+public class MyConstantTest {
+    public final boolean bool = true;   //1(0x01)
+    public final char c = 'A';          //65(0x41)
+    public final byte b = 66;           //66(0x42)
+    public final short s = 67;          //67(0x43)
+    public final int i = 68;            //68(0x44)
+}
