907f083f7cd1beacc603a0e597bb9e739d469c55
crazylong <316677841@qq.com>
2020-10-12 15:30:11 +0800

    mybatis-plus

--- mybatis-plus/src/test/java/com/coderlong/mybatisplus/OracleToMongodb.java
+++ mybatis-plus/src/test/java/com/coderlong/mybatisplus/OracleToMongodb.java
@@ -20,7 +20,6 @@ import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
